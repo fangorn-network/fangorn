bfdd1f07656c60589f1e8c5e5b85ed60d4596973
Coleman Irby <ckirbyee92@gmail.com>
2026-03-11 14:36:04 -0500

    updated delete to be a required function for storage provider

--- src/providers/storage/index.ts
+++ src/providers/storage/index.ts
@@ -3,5 +3,5 @@ export * from "./pinata/index.js";
 export default interface StorageProvider<T> {
 	store(data: T, metadata?: Record<string, unknown>): Promise<string>;
 	retrieve(cid: string): Promise<T>;
-	delete?(cid: string): Promise<void>;
+	delete(cid: string): Promise<void>;
 }
